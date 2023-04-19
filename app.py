feaa7af148bc82e6130205ac607aba9684543bd5
Tom Gilbert <tgilbert@scottlogic.com>
2023-04-19 22:05:31 +0100

    Refactored code to fix bug

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import secrets
 import os
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, session
 import random
 from datasets import load_dataset
 
 dataset = load_dataset("jxm/the_office_lines")
 speakers = list(set(example["speaker"] for example in dataset["train"]))
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 
 @app.route('/static/<path:path>')
@@ -17,21 +20,19 @@ def serve_static(path):
 
 @app.route('/')
 def quiz():
-    # Select a random line spoken by one of the characters in speakers
-    lines = [example["line_text"]
-             for example in dataset["train"] if example["speaker"] in speakers]
-    random_line = random.choice(lines)
+    # Select a random line spoken by any character in the dataset
+    example = random.choice(dataset["train"])
 
-    # Get the character who said the line
-    index = dataset["train"]["line_text"].index(random_line)
-    character = dataset["train"][index]["speaker"]
-
-    # Randomly select four characters, including the correct answer
-    answer_options = random.sample(speakers, k=3) + [character]
-    random.shuffle(answer_options)
+    # Save the line and character as session variables
+    session['line_text'] = example['line_text']
+    session['speaker'] = example['speaker']
 
     # Generate the question
-    question = f"Which character said the following line: '{random_line}'?"
+    question = f"Which character said the following line: '{example['line_text']}'?"
+
+    # Randomly select four characters, including the correct answer
+    answer_options = random.sample(speakers, k=3) + [example['speaker']]
+    random.shuffle(answer_options)
 
     # Render the template with the question and possible answers
     return render_template('quiz.html', question=question, answer_options=answer_options)
@@ -42,20 +43,15 @@ def check_answer():
     # Get the user's answer
     user_answer = request.form['answer']
 
-    # Select a random line spoken by one of the characters in speakers
-    lines = [example["line_text"]
-             for example in dataset["train"] if example["speaker"] in speakers]
-    random_line = random.choice(lines)
-
-    # Get the character who said the line
-    index = dataset["train"]["line_text"].index(random_line)
-    character = dataset["train"][index]["speaker"]
+    # Get the saved line and character from the session variables
+    line_text = session.get('line_text')
+    speaker = session.get('speaker')
 
     # Check if the user's answer is correct
-    if user_answer.lower() == character.lower():
+    if user_answer.lower() == speaker.lower():
         result = "Correct!"
     else:
-        result = f"Incorrect! The correct answer is {character}."
+        result = f"Incorrect! The correct answer is {speaker}."
 
     # Render the template with the result and a new question
     return render_template('results.html', result=result, speakers=speakers)
